11519017e9b726619e4d2a65f588f6d106b1c94f
Liksor <i20062005@gmail.com>
2026-07-09 22:43:32 +0300

    Task 1 ready

---
+++ DataTypeTask1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace DataTypeTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double initialDeposit = Convert.ToDouble(Console.ReadLine());
+            int years = Convert.ToInt32(Console.ReadLine());
+            double interestRate = Convert.ToDouble(Console.ReadLine());
+
+            string result = CalculationOfFunds(initialDeposit, years, interestRate);
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Расчёт сложного процента по годам.
+        /// </summary>
+        /// <returns>Строка с расчётом депозита для каждого года.</returns>
+        static string CalculationOfFunds (double initialDeposit , double years, double interestRate)
+        {
+            interestRate = (interestRate + 100) / 100;
+            var resultDeposit = new StringBuilder();
+            
+            for (int year = 1; year <= years; year++)
+            {
+                initialDeposit = initialDeposit * interestRate;
+                resultDeposit.Append($"Год {year}: {initialDeposit:f2} руб. \n");
+            }
+
+            return resultDeposit.ToString();
+        }
+    }
+}
