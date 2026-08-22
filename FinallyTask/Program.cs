4cc8a109ddec4861595ee452ddbff64a8b9eb97f
DESKTOP-UKDJPST\User <i20062005@gmail.com>
2026-08-22 21:13:25 +0300

    initial

---
+++ FinallyTask/Program.cs
@@ -0,0 +1,13 @@
+﻿using 
+
+namespace FinallyTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionStirng = "Server=localhost;Port=5432;Database=testdb;User Id=postgres;Password=200620"; 
+
+        }
+    }
+}
